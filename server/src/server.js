d95c364c1b2fb1ecd060dd329e96733f5de296bb
VitorVilelaM <vilelavitor305@gmail.com>
2023-05-26 16:08:39 -0300

    Finalização das rotas para todos os monitoramentos

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,89 @@
 const fastify = require("fastify")
 const snmp = require("net-snmp");
-
 const app = fastify()
-var oidsIps, ipDesejado;
+var oids, ipDesejado;
 
-app.get('/ips', (reply)=>{
-    oidsIps = ["1.3.6.1.2.1.4.3.0", "1.3.6.1.2.1.4.10.0"]
+app.get('/process', ()=>{   
+    oids = ["1.3.6.1.2.1.25.1.6.0"]
     ipDesejado = "127.0.0.1"
 
+    // Numero de processos dentro do processador
+
     var session = snmp.createSession (ipDesejado, "public");
-    session.get(oidsIps, function (error, varbinds) {
-        console.log(varbinds)
-        if (!error) {
+    session.get(oids, function (error, varbinds) {
+        if (error) {
             console.error (error);
-        } else {
-            console.log(session)
-            reply:{
-            }
+        }else{
+            console.log(varbinds);
+        }
+        session.close ();
+    });
+})
+
+app.get('/memory', ()=>{
+    oids = ["1.3.6.1.2.1.25.2.3.1.6.1", "1.3.6.1.2.1.25.2.2.0"]
+    ipDesejado = "127.0.0.1"
+
+    // 1º -A quantidade de armazenamento representada por esta entrada que é alocado, em unidades de Storage Allocation Units.
+    // 2° - A quantidade de memória física principal de leitura/gravação, tipicamente RAM, contida pelo host.
+    
+    var session = snmp.createSession (ipDesejado, "public");
+    session.get(oids, function (error, varbinds) {
+        if (error) {
+            console.error (error);
+        }else{
+            console.log(varbinds);
+        }
+        session.close ();
+    });
+})
+
+app.get('/ip', ()=>{
+    oids = ["1.3.6.1.2.1.4.3.0", "1.3.6.1.2.1.4.10.0"]
+    ipDesejado = "127.0.0.1"
+
+    // Numero de datagramas que entraram e sairam no meu PC, do protocolo IP
+
+    var session = snmp.createSession (ipDesejado, "public");
+    session.get(oids, function (error, varbinds) {
+        if (error) {
+            console.error (error);
+        }else{
+            console.log(varbinds);
+        }
+        session.close ();
+    });
+})
+
+app.get('/tcp', ()=>{
+    oids = ["1.3.6.1.2.1.6.10.0" , "1.3.6.1.2.1.6.11.0"]
+    ipDesejado = "127.0.0.1"
+
+    // Numero de datagramas que entraram e sairam no meu PC, do protocolo IP
+
+    var session = snmp.createSession (ipDesejado, "public");
+    session.get(oids, function (error, varbinds) {
+        if (error) {
+            console.error (error);
+        }else{
+            console.log(varbinds);
+        }
+        session.close ();
+    });
+})
+
+app.get('/udp', ()=>{
+    oids = ["1.3.6.1.2.1.7.1.0", "1.3.6.1.2.1.7.4.0"]
+    ipDesejado = "127.0.0.1"
+
+    // Numero de datagramas que entraram e sairam no meu PC, do protocolo IP
+
+    var session = snmp.createSession (ipDesejado, "public");
+    session.get(oids, function (error, varbinds) {
+        if (error) {
+            console.error (error);
+        }else{
+            console.log(varbinds);
         }
         session.close ();
     });
